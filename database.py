bd9efb22be2550fa96f8236c1db1d3f04468a845
Fakhrul Faiz <fakhrulfaiz201@gmail.com>
2024-02-28 02:43:57 +0800

    update database url

--- database.py
+++ database.py
@@ -3,13 +3,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread' : False})
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Mffaiz%40201@localhost/TodoApplicationDatabase'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Mffaiz%40201@localhost/TodoApplicationDatabase'
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Mffaiz%40201@127.0.0.1:3306/TodoApplicationDatabase'
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=10, max_overflow=20)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
